79f28e42973989e36c18d363e9fee6670efa6a9a
Martin Ionescu <martin.ionescu@nagarro.com>
2021-09-02 14:58:33 +0300

    Created project

---
+++ src/test/java/com/dropcox/DropcoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dropcox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropcoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
