dc27078d01694fbb8eb2183c8d16ef1d0eb8af75
Martin Ionescu <martin.ionescu@nagarro.com>
2021-09-19 14:39:45 +0300

    created migrations

---
+++ src/main/resources/db/migration/V1__InitialMigration.sql
@@ -0,0 +1,88 @@
+create table file_types
+(
+    id        bigint not null,
+    is_active bit,
+    type_name varchar(255),
+    uuid      varchar(255),
+    primary key (id)
+);
+create table folders_content
+(
+    id                 bigint not null,
+    added_date         datetime(6),
+    file_name          varchar(255),
+    last_modified_date datetime(6),
+    path               varchar(255),
+    file_size          double precision,
+    uuid               varchar(255),
+    file_creator_id    bigint,
+    file_type_model_id bigint,
+    parent_folder_id   bigint,
+    root_folder_id     bigint,
+    primary key (id)
+);
+create table folders_content_sub_files
+(
+    content_file_model_id bigint not null,
+    sub_files_id          bigint not null
+);
+create table hibernate_sequence
+(
+    next_val bigint
+);
+insert into hibernate_sequence
+values (1);
+create table root_folders
+(
+    id                bigint not null,
+    folder_name       varchar(255),
+    path              varchar(255),
+    is_shared         bit,
+    uuid              varchar(255),
+    folder_creator_id bigint,
+    primary key (id)
+);
+create table root_folders_files
+(
+    root_folder_model_id bigint not null,
+    files_id             bigint not null
+);
+create table users
+(
+    id        bigint not null,
+    email     varchar(255),
+    firstname varchar(255),
+    lastname  varchar(255),
+    password  varchar(255),
+    username  varchar(255),
+    uuid      varchar(255),
+    primary key (id)
+);
+alter table file_types
+    add constraint unique (type_name);
+alter table folders_content_sub_files
+    add constraint unique (sub_files_id);
+alter table root_folders_files
+    add constraint unique (files_id);
+alter table users
+    add constraint unique (email);
+alter table users
+    add constraint unique (username);
+alter table folders_content
+    add constraint foreign key (file_creator_id) references users (id);
+alter table folders_content
+    add constraint foreign key (file_type_model_id) references file_types (id);
+alter table folders_content
+    add constraint foreign key (parent_folder_id) references folders_content (id);
+alter table folders_content
+    add constraint foreign key (root_folder_id) references root_folders (id);
+alter table folders_content_sub_files
+    add constraint foreign key (sub_files_id) references folders_content (id);
+alter table folders_content_sub_files
+    add constraint foreign key (content_file_model_id) references folders_content (id);
+alter table root_folders
+    add constraint foreign key (folder_creator_id) references users (id);
+alter table root_folders_files
+    add constraint foreign key (files_id) references folders_content (id);
+alter table root_folders_files
+    add constraint foreign key (root_folder_model_id) references root_folders (id);
---
+++ src/main/resources/db/migration/V2__FileTypesMigration.sql
@@ -0,0 +1,4 @@
+INSERT INTO file_types (id, is_active, type_name, uuid)
+values (1, 1, 'directory', 'salut');
+UPDATE hibernate_sequence
+SET next_val = 2;
