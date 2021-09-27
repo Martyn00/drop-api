7e1a9201450295ce155a4b52ab37a3c8f73351c2
Martin Ionescu <martin.ionescu@nagarro.com>
2021-09-27 16:26:03 +0300

    added validation

---
+++ src/main/java/com/controller/dto/AddedUserDto.java
@@ -0,0 +1,17 @@
+package com.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AddedUserDto {
+    @JsonProperty(value = "userId")
+    String uuid;
+
+}
