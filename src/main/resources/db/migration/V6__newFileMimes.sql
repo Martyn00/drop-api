ef404f7ea5cd891ebcc63a5eab877b4a49f3a82d
Martin Ionescu <martinionescu11@gmail.com>
2021-10-24 14:13:40 +0300

    added image/png

--- src/main/resources/db/migration/V6__newFileMimes.sql
+++ src/main/resources/db/migration/V6__newFileMimes.sql
@@ -350,6 +350,11 @@ VALUES (119, 'audio/x-wav');
 INSERT INTO file_types_file_mimes (file_type_model_id, file_mime_id)
 VALUES (47, 119);
 
+INSERT INTO file_mimes (id, file_mime)
+VALUES (120, 'image/png');
+INSERT INTO file_types_file_mimes (file_type_model_id, file_mime_id)
+VALUES (6, 120);
+
 UPDATE hibernate_sequence
 SET next_val = 122;
 -- there are unused ids: 55, 59, 62, 101...
