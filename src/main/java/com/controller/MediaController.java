f7a6bbffb1b789ad5d90ebfd3bb616f7afacc189
Martin Ionescu <martinionescu11@gmail.com>
2021-10-24 16:59:46 +0300

    made audio and video streaming

--- src/main/java/com/controller/MediaController.java
+++ src/main/java/com/controller/MediaController.java
@@ -3,22 +3,32 @@ package com.controller;
 import com.controller.dto.MediaDto;
 import com.facade.MediaFacade;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @RestController(value = "media")
 @AllArgsConstructor
 public class MediaController {
+    public static final String AUDIO = "audio";
+    private static final int BYTE_RANGE = 128; // increase the byterange from here
+
     public static final String VIDEO = "video";
     public static final String IMAGE = "image";
+    @Value("${user.files}")
+    private static String BASIC_PATH;
     private final MediaFacade mediaFacade;
 
     @GetMapping(value = "/image/{fileUuid}")
@@ -32,14 +42,84 @@ public class MediaController {
                 .body(new InputStreamResource(inputStream));
     }
 
-    @GetMapping(value = "/video/{fileUuid}")
-    public ResponseEntity<InputStreamResource> getVideo(@PathVariable String fileUuid) throws FileNotFoundException {
-        MediaDto videoDto = mediaFacade.getMediaDto(fileUuid, VIDEO);
-        File video = new File(videoDto.getPath());
-        InputStream inputStream = new FileInputStream(video);
-        return ResponseEntity
-                .ok()
-                .contentType(videoDto.getMediaType())
-                .body(new InputStreamResource(inputStream));
+    @GetMapping(value = "/video/{filePath}")
+    public Mono<ResponseEntity<byte[]>> streamVideo(@RequestHeader(value = "Range", required = false) String httpRangeList,
+                                                    @PathVariable String filePath) {
+        //        can add find filePath from database
+        return Mono.just(getContent(BASIC_PATH + filePath, httpRangeList, VIDEO));
+    }
+
+    @GetMapping("/audio/{filePath}")
+    public Mono<ResponseEntity<byte[]>> streamAudio(@RequestHeader(value = "Range", required = false) String httpRangeList,
+                                                    @PathVariable String filePath) {
+        //        can add find filePath from database
+        return Mono.just(getContent(BASIC_PATH + filePath, httpRangeList, AUDIO));
+    }
+
+    private ResponseEntity<byte[]> getContent(String location, String range, String contentTypePrefix) {
+        long rangeStart = 0;
+        long rangeEnd;
+        byte[] data;
+        Long fileSize;
+        String fileType = location.substring(location.lastIndexOf(".") + 1);
+        try {
+            fileSize = Optional.ofNullable(location)
+                    .map(file -> Paths.get(location))
+                    .map(this::sizeFromFile)
+                    .orElse(0L);
+            if (range == null) {
+                return ResponseEntity.status(HttpStatus.OK)
+                        .header("Content-Type", contentTypePrefix + "/" + fileType)
+                        .header("Content-Length", String.valueOf(fileSize))
+                        .body(readByteRange(location, rangeStart, fileSize - 1));
+            }
+            String[] ranges = range.split("-");
+            rangeStart = Long.parseLong(ranges[0].substring(6));
+            if (ranges.length > 1) {
+                rangeEnd = Long.parseLong(ranges[1]);
+            } else {
+                rangeEnd = fileSize - 1;
+            }
+            if (fileSize < rangeEnd) {
+                rangeEnd = fileSize - 1;
+            }
+            data = readByteRange(location, rangeStart, rangeEnd);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .header("Content-Type", contentTypePrefix +
+                        "/" + fileType)
+                .header("Accept-Ranges", "bytes")
+                .header("Content-Length", contentLength)
+                .header("Content-Range", "bytes" + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
+                .body(data);
+    }
+
+    public byte[] readByteRange(String location, long start, long end) throws IOException {
+        Path path = Paths.get(location);
+        try (InputStream inputStream = (Files.newInputStream(path));
+             ByteArrayOutputStream bufferedOutputStream = new ByteArrayOutputStream()) {
+            byte[] data = new byte[BYTE_RANGE];
+            int nRead;
+            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                bufferedOutputStream.write(data, 0, nRead);
+            }
+            bufferedOutputStream.flush();
+            byte[] result = new byte[(int) (end - start) + 1];
+            System.arraycopy(bufferedOutputStream.toByteArray(), (int) start, result, 0, result.length);
+            return result;
+        }
+    }
+
+    private Long sizeFromFile(Path path) {
+        try {
+            return Files.size(path);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return 0L;
     }
 }
