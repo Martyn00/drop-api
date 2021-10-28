be79ec2c57875d3cbcfe5bd2354d5582d9d4d4f4
Martin Ionescu <martinionescu11@gmail.com>
2021-10-28 14:59:00 +0300

    Made audio streaming and video streaming

--- src/main/java/com/controller/MediaController.java
+++ src/main/java/com/controller/MediaController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import com.controller.dto.MediaDto;
 import com.facade.MediaFacade;
+import com.foldermanipulation.FileService;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -10,12 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
@@ -24,9 +22,8 @@ import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/media")
 public class MediaController {
+    private final FileService fileService;
     public static final String AUDIO = "audio";
-    private static final int BYTE_RANGE = 128; // increase the byterange from here
-
     public static final String VIDEO = "video";
     public static final String IMAGE = "image";
 
@@ -38,11 +35,6 @@ public class MediaController {
     public ResponseEntity<Resource> getImage(@PathVariable String fileUuid) throws IOException {
         MediaDto imageDto = mediaFacade.getMediaDto(fileUuid, IMAGE);
         File image = new File(imageDto.getPath());
-//        InputStream inputStream = new FileInputStream(image);
-//        return ResponseEntity
-//                .ok()
-//                .contentType(imageDto.getMediaType())
-//                .body(new InputStreamResource(inputStream));
         final ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(image.getPath())));
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -50,10 +42,9 @@ public class MediaController {
                 .body(inputStream);
     }
 
-    @GetMapping(value = "/video/{filePath}")
+    @GetMapping(value = "/video")
     public Mono<ResponseEntity<byte[]>> streamVideo(@RequestHeader(value = "Range", required = false) String httpRangeList,
-                                                    @PathVariable String filePath) {
-        //        can add find filePath from database
+                                                    @RequestParam String filePath) {
         return Mono.just(getContent(BASIC_PATH + filePath, httpRangeList, VIDEO));
     }
 
@@ -71,15 +62,15 @@ public class MediaController {
         Long fileSize;
         String fileType = location.substring(location.lastIndexOf(".") + 1);
         try {
-            fileSize = Optional.ofNullable(location)
+            fileSize = Optional.of(location)
                     .map(file -> Paths.get(location))
-                    .map(this::sizeFromFile)
+                    .map(fileService::sizeFromFile)
                     .orElse(0L);
             if (range == null) {
                 return ResponseEntity.status(HttpStatus.OK)
                         .header("Content-Type", contentTypePrefix + "/" + fileType)
                         .header("Content-Length", String.valueOf(fileSize))
-                        .body(readByteRange(location, rangeStart, fileSize - 1));
+                        .body(fileService.readByteRange(location, rangeStart, fileSize - 1));
             }
             String[] ranges = range.split("-");
             rangeStart = Long.parseLong(ranges[0].substring(6));
@@ -91,7 +82,7 @@ public class MediaController {
             if (fileSize < rangeEnd) {
                 rangeEnd = fileSize - 1;
             }
-            data = readByteRange(location, rangeStart, rangeEnd);
+            data = fileService.readByteRange(location, rangeStart, rangeEnd);
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -106,28 +97,4 @@ public class MediaController {
                 .body(data);
     }
 
-    public byte[] readByteRange(String location, long start, long end) throws IOException {
-        Path path = Paths.get(location);
-        try (InputStream inputStream = (Files.newInputStream(path));
-             ByteArrayOutputStream bufferedOutputStream = new ByteArrayOutputStream()) {
-            byte[] data = new byte[BYTE_RANGE];
-            int nRead;
-            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                bufferedOutputStream.write(data, 0, nRead);
-            }
-            bufferedOutputStream.flush();
-            byte[] result = new byte[(int) (end - start) + 1];
-            System.arraycopy(bufferedOutputStream.toByteArray(), (int) start, result, 0, result.length);
-            return result;
-        }
-    }
-
-    private Long sizeFromFile(Path path) {
-        try {
-            return Files.size(path);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        return 0L;
-    }
 }
--- src/main/java/com/foldermanipulation/FileService.java
+++ src/main/java/com/foldermanipulation/FileService.java
@@ -18,6 +18,8 @@ public class FileService {
     @Value("${user.files}")
     private String BASIC_PATH;
 
+    private static final int BYTE_RANGE = 512;
+
     public void uploadFile(MultipartFile file, String path) {
         path = BASIC_PATH + path;
         InputStream in = getInputStream(file);
@@ -55,5 +57,30 @@ public class FileService {
         } catch (IOException e) {
             throw new FolderException("Folder or file cannot be copied");
         }
+    }
+
+    public byte[] readByteRange(String location, long start, long end) throws IOException {
+        Path path = Paths.get(location);
+        try (InputStream inputStream = (Files.newInputStream(path));
+             ByteArrayOutputStream bufferedOutputStream = new ByteArrayOutputStream()) {
+            byte[] data = new byte[BYTE_RANGE];
+            int nRead;
+            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                bufferedOutputStream.write(data, 0, nRead);
+            }
+            bufferedOutputStream.flush();
+            byte[] result = new byte[(int) (end - start) + 1];
+            System.arraycopy(bufferedOutputStream.toByteArray(), (int) start, result, 0, result.length);
+            return result;
+        }
+    }
+
+    public Long sizeFromFile(Path path) {
+        try {
+            return Files.size(path);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return 0L;
     }
 }
