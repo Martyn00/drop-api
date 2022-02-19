348a54ed5fecb3766d10bbb1da12295600af46ec
Martin Ionescu <martinionescu11@gmail.com>
2022-02-19 17:35:02 +0200

    webosckets

---
+++ src/main/java/com/configuration/WebSocketSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.configuration;
+
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .nullDestMatcher().authenticated()
+                .simpSubscribeDestMatchers("/topic").permitAll();
+
+
+    }
+}
