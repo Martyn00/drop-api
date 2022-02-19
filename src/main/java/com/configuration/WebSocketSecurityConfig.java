f2dfe25177736bbb5163aa507242daa578d32e2f
Martin Ionescu <martinionescu11@gmail.com>
2022-02-19 17:35:41 +0200

    web socket security config

--- src/main/java/com/configuration/WebSocketSecurityConfig.java
+++ src/main/java/com/configuration/WebSocketSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
+@Configuration
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
         messages
